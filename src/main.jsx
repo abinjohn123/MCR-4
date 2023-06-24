2114a6289d6df6b21258fbf6ce53ada775e9b988
Abin John <abinjohnanto@gmail.com>
2023-06-24 22:21:40 +0530

    add routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App.jsx';
 import './index.scss';
@@ -7,8 +8,10 @@ import AppProvider from '../AppContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <Router>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </Router>
   </React.StrictMode>
 );
