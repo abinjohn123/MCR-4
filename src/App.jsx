93aafa219c1a65c5a982cb475efec4eb2a4d3e75
Abin John <abinjohnanto@gmail.com>
2023-06-24 19:19:15 +0530

    feat: basic app header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
 
+import Navbar from './Components/Navbar';
 import Sidebar from './Components/Sidebar';
 import './App.scss';
 
 function App() {
   return (
     <div className="layout">
+      <Navbar />
       <Sidebar />
       <div>
         <p>App comes here</p>
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <header className="app-header">
+      <h1>MyForum</h1>
+    </header>
+  );
+};
+
+export default Navbar;
